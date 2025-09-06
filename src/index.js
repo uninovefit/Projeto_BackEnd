bf0c036ae9f2295bc92856822bbcc2fdab850843
adailton.santos <adailtonaparecido69@gmail.com>
2025-09-06 01:45:54 -0300

    Fix: Remove Handlebars, pois não será utilizado

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const app = express();
-const handlebars = require('express-handlebars');
-const path = require('path');
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
-    res.render('home', { title: 'Home Page' });
+    res.send('Hello World!');
 });
 
 const port = process.env.PORT || 8081;
